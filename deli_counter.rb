dbeb8235356044c13cfaa510351d6059c717e401
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-23 16:20:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,8 @@ def line(katz_deli)
   end
   puts "#{message}"
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end

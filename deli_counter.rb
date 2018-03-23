555901a65a92835bb30358fd616d1d9d5ef06b33
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-23 16:31:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,10 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli == 0
+    puts "There is nobocy waiting to be served!"
+  else
+    puts "Currently serving #{name}."  
+end
